6364e7f2570ca9b1b3b5d1d18aa45c370c063fc3
alfin <fachrizalalfin@gmail.com>
2024-04-30 09:07:46 +0700

    template baru

--- assets/js/formatrupiah.js
+++
@@ -1,3 +0,0 @@
-function formatRupiah(number) {
-    return number.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-}
--- component/trigger_filter.js
+++
@@ -1,8 +0,0 @@
-document.write(`
-	<button class="btn btn-outline-dark w-100 rounded-0">
-		<i class="bi bi-arrow-counterclockwise"></i> Atur Ulang
-	</button>
-	<button class="btn btn-dark w-100 rounded-0">
-		Terapkan <i class="bi bi-chevron-right"></i>
-	</button> 
-	`);
